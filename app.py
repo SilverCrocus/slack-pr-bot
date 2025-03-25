9107f394915228d909bd1902ea4f4029613dee1b
Hivin Diyagama <49328442+SilverCrocus@users.noreply.github.com>
2025-03-25 16:50:44 +1100

    updated slash command

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import logging
 from flask import Flask
 from dotenv import load_dotenv
 from pr_review_bot import pr_webhook
-from reaction_handler import slack_events, handle_slash_command
+from reaction_handler import slack_events, handle_slash_command as handle_pr_command
 from slash_commands import handle_slash_command as handle_help_commands
 
 # Load environment variables
@@ -22,7 +22,7 @@ app = Flask(__name__)
 # Register routes
 app.add_url_rule('/webhook/pr', view_func=pr_webhook, methods=['POST'])
 app.add_url_rule('/slack/events', view_func=slack_events, methods=['POST'])
-app.add_url_rule('/slack/commands', view_func=handle_slash_command, methods=['POST'])
+app.add_url_rule('/slack/commands', view_func=handle_pr_command, methods=['POST'])
 app.add_url_rule('/slack/help-commands', view_func=handle_help_commands, methods=['POST'])
 
 if __name__ == '__main__':
